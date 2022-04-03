5e9ae770ee7a20cf010c434850266472aa54ca64
SwatiRaghav <swaticraghav@gmail.com>
2022-04-03 11:16:01 +0530

    First Commit

---
+++ src/test/java/com/core/services/RestwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
